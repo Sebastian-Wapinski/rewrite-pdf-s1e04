22c996d165ec78ef11790f86b07bac42069921b0
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-03-13 19:54:07 +0100

    start 0 a nie 1 w Math.random

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ let userNumber;
 let counter = 0;
 
 while (userNumber !== randomNumber) {
-    userNumber = parseInt(prompt('Zgadnij liczbę 1-5!'));
+    userNumber = parseInt(prompt('Zgadnij liczbę 0-5!'));
     counter+=1;
 }
 
