7ae3376140e0a047268d3f5402ed50f67a309e0b
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-03-13 18:08:48 +0100

    03 #3 porównanie

--- js/app.js
+++ js/app.js
@@ -1,11 +1,22 @@
-let x = 12;
-x += '0';
-console.log(x);
-x -= 100;
-console.log(x);
-x *= 'a';
-console.log(x);
-x = 1000;
-console.log(x);
-x /= 'b';
-console.log(x);
+const x = 5;
+
+console.log(x == "5");
+
+console.log(x === "5");
+
+console.log(x !== "5");
+
+console.log(x !== 5);
+
+console.log("0" == 0);
+
+console.log(0 == false);
+
+console.log(false == '0')
+
+console.log("0" === 0);
+
+console.log(0 === false);
+
+console.log(false === '0')
+
