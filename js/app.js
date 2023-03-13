13cc8da1fdce663e1841d06a4b455a580bd74041
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-03-13 12:20:38 +0100

    01 html z js

---
+++ js/app.js
@@ -0,0 +1,12 @@
+console.log('JavaScript');
+console.log('jest');
+console.log('COOL');
+
+const str = 'wartość typu string';
+
+const sum = 1 + 2; //komentarz
+
+console.log('wyświetlam', str);
+console.log('wyświetlam również liczbę', sum);
+
+alert('JS dizała!'); //komunikat
