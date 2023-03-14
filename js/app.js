e437bfd03b48eb24393d56155604ae93b8ea5e2f
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-03-14 14:42:35 +0100

    06 #3 shorten

--- js/app.js
+++ js/app.js
@@ -1,10 +1,8 @@
-function getUserInput(messageForUser) {
+const fn = function getUserInput(messageForUser) {
     const userInput = prompt(messageForUser);
 
     return userInput;
 }
-
-const fn = getUserInput;
 
 const inputFromUser = fn('Ile masz lat?');
 console.log(inputFromUser);
