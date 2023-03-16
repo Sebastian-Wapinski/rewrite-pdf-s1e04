b020384ac79dc75e991e78c7f9a62915a905a670
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-03-16 16:30:06 +0100

    09 #1 1

--- js/app.js
+++ js/app.js
@@ -1,11 +1,8 @@
-const arr = [];
+const arr = new Array(0);
 
-const names = ['Jan', 'Anna', 'Kasia'];
+const names = new Array('Jan', 'Anna', 'Kasia');
 
 console.log(names[0]);
 
 console.log(names.length);
 
-const lastIndex = names.length - 1;
-
-console.log(names[lastIndex]);
