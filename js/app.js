936f28bb18668b76a927486f3dbda5d6d88518bc
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-03-17 11:36:20 +0100

    09 #2 evenNumbers

--- js/app.js
+++ js/app.js
@@ -1,8 +1,23 @@
 const numbers = [1, 2, 3, 4, 5];
 
 const sum = getSum(numbers);
-
 console.log(sum);
+
+const evenNumbers = getEvenNumbers(numbers);
+const sum2 = getSum(evenNumbers);
+console.log(sum2);
+
+function getEvenNumbers(arr) {
+    const evenNumbers = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (!(arr[i] % 2)) {
+            evenNumbers.push(arr[i]);
+        }
+    }
+    return evenNumbers;
+}
+
+
 
 function getSum(arr) {
     let sum = 0;
