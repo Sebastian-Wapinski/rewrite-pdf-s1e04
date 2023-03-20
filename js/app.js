0fbf986b364ef51fe5994c2bb16a426fd32e3cc4
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-03-20 11:16:58 +0100

    10 #5 obiekty wbudowane

--- js/app.js
+++ js/app.js
@@ -1,17 +1,10 @@
-const travelPrice = {
-    flight: 591,
-    hotel: 1400,
-    excursion: 421,
-    other: 120,
-}
-const totalPrice = getTotalPrice(travelPrice);
-console.log(totalPrice);
+const now = new Date();
 
-function getTotalPrice(priceObject) {
-    const values = Object.values(priceObject);
-    const totalPrice = values.reduce(function(acc, price) {
-        return acc + price;
-    }, 0)
-
-    return totalPrice;
-}
+console.log(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate(),
+    now.getHours(),
+    now.getMinutes(),
+    now.getSeconds(),
+);
