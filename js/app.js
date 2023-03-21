f6179bf82b9397f5cf2be19d0929c6aeac50c4d1
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-03-21 13:07:41 +0100

    F7 This

--- js/app.js
+++ js/app.js
@@ -3,14 +3,26 @@ const product = {
     type: 'book',
     price: {
         value: 49,
+    },
+    getName() {
+        let value = null;
+        
+        const self = this;
+        console.log(this.name);
+
+        function fn() {
+            console.log(this);
+            value = self.name;
+        }
+
+        fn();
+        return value;
     }
 }
 
-const keys = Object.keys(product);
-const values = Object.values(product);
+console.log(product.getName());
 
-console.log(keys, values);
-
-//Szybkie przypisanie właściwości i wartości do tablic 
-//na których potem możemy pracować
+// z racji tego że this odwołuje się do obiektu przed obiektem w którym jest wywoływany 
+// umieszczając go w funkcji wewnątrz funkcji wewnątrz obiektu będzie wskazywał na window
+// i dlatego musimy stworzyć najpierw zmienną
 
