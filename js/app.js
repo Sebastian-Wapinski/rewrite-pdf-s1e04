0e98a03c81c30a95c37e9c6e636bc69b2730bd2c
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-03-21 17:44:48 +0100

    11 Konstruktory

--- js/app.js
+++ js/app.js
@@ -1,23 +1,12 @@
-const product = {
-    name: 'Podstawy JS',
-    type: 'book',
-    price: 49,
-    q: 1,
-};
+function Student(initFirstName, initLastName) {
+    this.firstName = initFirstName;
+    this.lastName = initLastName;
+}
 
-const cart = [
-    {...product, price: 99},
-    {...product, price: 79},
-    {...product, q: 2},
-];
+const student1 = new Student('Marek', 'Nowak');
+const student2 = new Student('Anna', 'Kowal');
 
-console.log(cart);
-
-let initialValue = 0;
-const sum = cart.filter(function(el) {
-    return el.q > 1;
-}).reduce(function(acc, el) {
-    return acc + el.price;
-}, initialValue);
-
-console.log(sum);
+console.log(
+    student1,
+    student2,
+)
