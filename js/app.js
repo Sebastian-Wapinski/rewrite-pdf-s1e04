c1d2feff5c4ba9a615b5e53199096ba33619e3d5
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-03-21 13:16:07 +0100

    F8 łączenie rozwiązań

--- js/app.js
+++ js/app.js
@@ -1,29 +1,11 @@
 const product = {
     name: 'Podstawy JS',
     type: 'book',
-    price: {
-        value: 49,
-    },
-    getName() {
-        let value = null;
-        
-        const self = this;
-        console.log(this.name);
-
-        function fn() {
-            console.log(this);
-            value = self.name;
-        }
-
-        fn();
-        return value;
-    }
+    price: [49, 39],
 }
 
-console.log(product.getName());
+product.price.forEach(function(price) {
+    console.log(price);
+})
 
-// z racji tego że this odwołuje się do obiektu przed obiektem w którym jest wywoływany 
-// umieszczając go w funkcji wewnątrz funkcji wewnątrz obiektu będzie wskazywał na window
-// i dlatego musimy stworzyć najpierw zmienną
-// dlatego się mówi że zależy od kontekstu
 
