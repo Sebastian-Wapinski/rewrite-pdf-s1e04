3ae33cb683ca3a1f923d9fcbf701e6a11e21dcc6
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-03-21 15:06:50 +0100

    F11 reduce + filter

--- js/app.js
+++ js/app.js
@@ -2,18 +2,21 @@ const product = {
     name: 'Podstawy JS',
     type: 'book',
     price: 49,
+    q: 1,
 };
 
 const cart = [
     {...product, price: 99},
     {...product, price: 79},
-    {...product},
+    {...product, q: 2},
 ];
 
 console.log(cart);
 
 let initialValue = 0;
-const sum = cart.reduce(function(acc, el) {
+const sum = cart.filter(function(el) {
+    return el.q > 1;
+}).reduce(function(acc, el) {
     return acc + el.price;
 }, initialValue);
 
